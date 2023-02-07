fed37a6f717af2a3ae482217cde165b6ea252c49
Lisacyj0102 <s1110169@my.cmsh.cyc.edu.tw>
2023-02-07 17:58:50 +0800

    correct intake

--- src/main/java/frc/robot/component/Intake.java
+++ src/main/java/frc/robot/component/Intake.java
@@ -8,14 +8,12 @@ import frc.robot.Robot;
 
 public class Intake {
     private static Compressor com ;
-    private static DoubleSolenoid solleft;
-    private static DoubleSolenoid solright;
-    private static boolean solleft_Forward = true;
-    private static boolean solright_Forward = true;
+    private static DoubleSolenoid sol;
+    private static boolean sol_Forward = true;
     public static void init() {
     com = new Compressor(null);
-    solleft = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 0);
-    solright = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 0);
+    sol = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0, 0);
+    
     }
     public static void teleop() {
         if(Robot.xbox.getYButton()){
@@ -24,28 +22,16 @@ public class Intake {
             com.disable();
         }
         if(Robot.xbox.getBButtonPressed()){
-            solleft_Forward = !solleft_Forward;
+            sol_Forward = !sol_Forward;
         }
         if (Robot.xbox.getBButton()) {
-            if (solleft_Forward== false) {
-                solleft.set(Value.kForward);
+            if (sol_Forward== false) {
+                sol.set(Value.kForward);
             } else {
-                solleft.set(Value.kReverse);
+                sol.set(Value.kReverse);
             }
         } else {
-            solleft.set(Value.kOff);
-        }
-        if(Robot.xbox.getXButtonPressed()){
-            solright_Forward = !solright_Forward;
-        }
-        if(Robot.xbox.getXButton()){
-            if(solright_Forward==false){
-                solright.set(Value.kForward);
-            }else{
-                solright.set(Value.kReverse);
-            }
-        }else{
-            solright.set(Value.kOff);
+            sol.set(Value.kOff);
         }
     }
 }
