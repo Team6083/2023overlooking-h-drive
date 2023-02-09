036cb818f3d83b8a684bdcfd69a1b64e4f283e98
William960321 <s1110270@my.cmsh.cyc.edu.tw>
2023-02-09 18:51:45 +0800

    limelight

--- src/main/java/frc/robot/component/Limelight.java
+++ src/main/java/frc/robot/component/Limelight.java
@@ -1,6 +1,111 @@
 package frc.robot.component;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
+
 public class Limelight {
-    public static void init() {}
-    public static void teleop() {}
+    private static double tx;
+    private static double tv;
+    private static NetworkTable table;
+    private static PIDController pid;
+    protected static boolean Limelight_SwitchLeft = false;
+    protected static boolean Limelight_SwitchRight = false;
+    private static double hDriveSpeed;
+    private static final double hDrive_MaxSpeed = 0.8;
+    private static final double a = 0.0; // to be confirmed
+
+    public static void init() {
+        pid = new PIDController(0.04, 0.001, 0.0001); // to be confirmed
+        setLEDMode(1);
+        setCamMode(1);
+    }
+
+    public static void teleop() {
+        if (Robot.xbox.getXButtonPressed()) { // to be confirmed
+            Limelight_SwitchLeft = !Limelight_SwitchLeft;
+            if (Limelight_SwitchLeft) {
+                Limelight_SwitchRight = false;
+            }
+        }
+        if (Robot.xbox.getYButtonPressed()) { // to be confirmed
+            Limelight_SwitchRight = !Limelight_SwitchRight;
+            if (Limelight_SwitchRight) {
+                Limelight_SwitchLeft = false;
+            }
+        }
+        if (Limelight_SwitchLeft || Limelight_SwitchRight) {
+            setLEDMode(0);
+            setCamMode(0);
+            aiming();
+        } else {
+            setLEDMode(1);
+            setCamMode(1);
+        }
+        putDashboard();
+    }
+
+    public static void aiming() {
+        table = NetworkTableInstance.getDefault().getTable("limelight");
+        tx = table.getEntry("tx").getDouble(0) - a;
+        tv = table.getEntry("tv").getDouble(0);
+        hDriveSpeed = pid.calculate(tx, 0.0);
+        if (tv > 0) {
+            if (hDriveSpeed > hDrive_MaxSpeed) {
+                hDriveSpeed = hDrive_MaxSpeed;
+            } else if (hDriveSpeed < -hDrive_MaxSpeed) {
+                hDriveSpeed = -hDrive_MaxSpeed;
+            }
+            DriveBase.middleMotor.set(hDriveSpeed);
+        } else {
+            finding();
+        }
+
+    }
+
+    public static void finding() {
+        if (Limelight_SwitchLeft) {
+            DriveBase.middleMotor.set(0.5);
+        }
+        if (Limelight_SwitchRight) {
+            DriveBase.middleMotor.set(-0.5);
+        }
+
+    }
+
+    public static void putDashboard() {
+        SmartDashboard.putBoolean("left limelight switch", Limelight_SwitchLeft);
+        SmartDashboard.putBoolean("right limelight switch", Limelight_SwitchRight);
+    }
+
+    /**
+     * 0 use the LED Mode set in the current pipeline
+     * 1 force off
+     * 2 force blink
+     * 3 force on
+     * 
+     * @param ModeNumber use to set LED mode
+     */
+
+    public static void setLEDMode(int ModeNumber) {
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(ModeNumber);
+    }
+
+    /**
+     * 0 Vision processor
+     * 1 Driver Camera (Increases exposure, disables vision processing)
+     *
+     * @param CamNumber use to set Cam mode
+     */
+
+    public static void setCamMode(int CamNumber) {
+        NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(CamNumber);
+    }
+
+    public static void disabledInit() {
+        setLEDMode(1);
+        setCamMode(1);
+    }
 }
