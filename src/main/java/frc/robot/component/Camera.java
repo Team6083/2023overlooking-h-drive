b850d7d84ed1552cdf653ba39c6b0fee2e920041
Lisacyj0102 <s1110169@my.cmsh.cyc.edu.tw>
2023-02-14 19:18:54 +0800

    add port

--- src/main/java/frc/robot/component/Camera.java
+++ src/main/java/frc/robot/component/Camera.java
@@ -6,7 +6,7 @@ import edu.wpi.first.cscore.UsbCamera;
 public class Camera {
     private static UsbCamera camera;
     public static void init(){
-        camera = CameraServer.startAutomaticCapture();
+        camera = CameraServer.startAutomaticCapture(0);
         camera.setFPS(24);
     }
 
